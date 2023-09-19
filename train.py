f56431686be29a43eca82c7e024fa53b82ad0ca2
Sri Ram M S <sriram.mssn@gmail.com>
2023-09-19 10:17:48 +0530

    Update train.py

--- train.py
+++ train.py
@@ -4,18 +4,14 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 import pandas as pd
 import numpy as np
-import joblib
-import os
+
 iris_df = load_iris()
 X = iris_df.data
 y = iris_df.target
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size = 0.1)
-data_dir = "./data"
-model_dir = "./model"
 model = LogisticRegression()
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
-joblib.dump(model,os.path.join(model_dir,"logistic_model.joblib"))
 
 with open("metrics.txt",'w') as fw:
   fw.write(f"Mean Squared Error of the current model:{classification_report(y_test,y_pred)}")
